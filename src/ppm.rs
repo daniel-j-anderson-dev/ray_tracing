46f32e20d569a4fc522297f9cd0d7720097b5401
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-08-14 11:06:06 -0600

    cargo fmt

--- src/ppm.rs
+++ src/ppm.rs
@@ -2,7 +2,10 @@ use crate::CLEAR_LINE;
 
 use std::num::NonZeroUsize;
 
-fn generate_red_green_gradient_ppm(image_width: NonZeroUsize, image_height: NonZeroUsize) -> String {
+fn generate_red_green_gradient_ppm(
+    image_width: NonZeroUsize,
+    image_height: NonZeroUsize,
+) -> String {
     // header
     let mut ppm = format!("P3\n{} {}\n255\n", image_width, image_height);
 
@@ -13,7 +16,6 @@ fn generate_red_green_gradient_ppm(image_width: NonZeroUsize, image_height: NonZ
         eprint!("{CLEAR_LINE}{}", image_height.get() - row_index);
 
         for column_index in 0..image_width.get() {
-            
             let red = column_index % 256;
             let green = row_index % 256;
 
@@ -32,7 +34,11 @@ fn save_red_green_gradient_ppm() {
 
     let size = NonZeroUsize::new(256).unwrap();
 
-    eprintln!("generating {}x{} red green gradient", size.get(), size.get());
+    eprintln!(
+        "generating {}x{} red green gradient",
+        size.get(),
+        size.get()
+    );
 
     let gradient_image = generate_red_green_gradient_ppm(size, size);
     let gradient_path = format!("{}x{}_red_green_gradient.ppm", size.get(), size.get());
