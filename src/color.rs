377ecc8d4ec57ce4592de23d93c35ac9da7e2ffb
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-06-14 09:41:03 -0600

    made fields public to simplify and moved ppm methods

--- src/color.rs
+++ src/color.rs
@@ -1,64 +1,25 @@
-use crate::{percent::Percent};
+pub mod conversion;
 
-mod inter_color_conversion;
-
-/// Represents a ratio of red, green, and blue color channels. The fields are brightness percentages (0.0 black, 1.0 full color).
-/// ## Invariant
-/// Each color channel will be in the interval `[0.0, 1.0]`
+/// Represents a ratio of red, green, and blue color channels.
+/// The fields are brightness percentages (0.0 no color, 1.0 full color).
 #[derive(Debug, Default, Clone, Copy, PartialEq, PartialOrd)]
 pub struct RgbPercent {
-    red: Percent,
-    green: Percent,
-    blue: Percent,
-}
-impl RgbPercent {
-    pub fn red(&self) -> f64 {
-        self.red.get()
-    }
-    pub fn green(&self) -> f64 {
-        self.green.get()
-    }
-    pub fn blue(&self) -> f64 {
-        self.blue.get()
-    }
+    pub red: f64,
+    pub green: f64,
+    pub blue: f64,
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct RgbU8 {
-    red: u8,
-    green: u8,
-    blue: u8,
-}
-impl RgbU8 {
-    pub fn red(&self) -> u8 {
-        self.red
-    }
-    pub fn green(&self) -> u8 {
-        self.green
-    }
-    pub fn blue(&self) -> u8 {
-        self.blue
-    }
+pub struct Rgb8 {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct RgbaU8 {
-    red: u8,
-    green: u8,
-    blue: u8,
-    alpha: u8,
-}
-impl RgbaU8 {
-    pub fn red(&self) -> u8 {
-        self.red
-    }
-    pub fn green(&self) -> u8 {
-        self.green
-    }
-    pub fn blue(&self) -> u8 {
-        self.blue
-    }
-    pub fn alpha(&self) -> u8 {
-        self.alpha
-    }
+pub struct Rgba8 {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+    pub alpha: u8,
 }
