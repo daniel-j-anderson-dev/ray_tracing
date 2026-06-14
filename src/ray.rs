d9a68e6749d1490c3afaee8fd6ec9da62b73d526
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-06-14 11:31:12 -0600

    made fields public

--- src/ray.rs
+++ src/ray.rs
@@ -2,20 +2,10 @@ use crate::r3_vector::R3Vector;
 
 #[derive(Debug, Default, Clone, Copy, PartialEq)]
 pub struct Ray {
-    origin: R3Vector,
-    direction: R3Vector,
+    pub origin: R3Vector,
+    pub direction: R3Vector,
 }
 impl Ray {
-    pub const fn new(origin: R3Vector, direction: R3Vector) -> Self {
-        Self { origin, direction }
-    }
-    pub const fn origin(&self) -> R3Vector {
-        self.origin
-    }
-    pub const fn direction(&self) -> R3Vector {
-        self.direction
-    }
-
     pub fn at(&self, t: f64) -> R3Vector {
         self.origin + (t * self.direction)
     }
