c09c1496e72e862266a7dd24e7153291e86d583e
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-06-14 09:41:40 -0600

    removed thiserror

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,17 @@
-use thiserror::Error;
-
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Error)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Error {
-    #[error("The Percent type's value must be in the interval: [0.0, 1.0]")]
     PercentageOutOfRange,
 }
+impl Error {}
+impl core::fmt::Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        use Error::*;
+        match self {
+            PercentageOutOfRange => write!(
+                f,
+                "The Percent type's value must be in the interval: [0.0, 1.0]"
+            ),
+        }
+    }
+}
+impl core::error::Error for Error {}
