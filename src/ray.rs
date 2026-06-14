98387161555b31e43c89c9174888b0e54982a794
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-06-14 11:31:22 -0600

    added doc comment

--- src/ray.rs
+++ src/ray.rs
@@ -6,6 +6,8 @@ pub struct Ray {
     pub direction: R3Vector,
 }
 impl Ray {
+    /// `P(t) = A + tB` where `A` is the origin vector and `B` is the direction vector
+    /// ![](https://raytracing.github.io/images/fig-1.02-lerp.jpg)
     pub fn at(&self, t: f64) -> R3Vector {
         self.origin + (t * self.direction)
     }
